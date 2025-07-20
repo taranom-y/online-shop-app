6c99030b77f42672e01bd4e1499c361eadf94895
taranom_y <tarannom.yousefi98@gmail.com>
2025-07-20 21:27:24 +0330

    feat: add Loading page for product list

--- components/ui/productList.tsx
+++ components/ui/productList.tsx
@@ -1,26 +1,29 @@
 import Image from "next/image";
 import { Card, CardContent } from "./card";
 import { Button } from "./button";
-import { useProductStore } from "@/store/productStore";
-import useProducts from "@/lib/hooks/useProducts";
+import { useProductStore } from "@/store/ProductStore";
+import useProducts from "@/hooks/useProducts";
+import { useCartStore } from "@/store/CartStore";
+import IsLoading from "@/app/Loading";
 
 export default function ProductList() {
   const { isLoading, isError, error } = useProducts();
 
   const { products, search, category } = useProductStore((state) => state);
-
+  const addToCart = useCartStore((state) => state.addItems);
   const filtered = products.filter((p) => {
     const matchCategory = category === "all" || p.category === category;
     const matchSearch = p.title.toLowerCase().includes(search.toLowerCase());
     return matchCategory && matchSearch;
   });
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading) return <IsLoading />;
+
   if (isError) return <p>Error: {(error as Error).message}</p>;
 
   return (
-    <section className="py-16">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+    <section className="py-16 ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10">
         {filtered.map((product) => (
           <Card key={product.id} className="p-4 flex flex-col">
             <Image
@@ -28,16 +31,18 @@ export default function ProductList() {
               alt={product.title}
               width={200}
               height={200}
-              className="h-48 w-full object-contain mb-4"
+              className="h-40 w-full object-contain mb-4"
             />
             <CardContent className="flex-1">
-              <h2 className="font-semibold text-lg mb-2">{product.title}</h2>
-              <p className="text-gray-600 mb-2">${product.price}</p>
+              <h2 className="font-semibold text-lg ">{product.title}</h2>
+              <p className="text-gray-600 ">${product.price}</p>
               <p className="text-xs text-muted-foreground">
                 {product.category}
               </p>
             </CardContent>
-            <Button className="mt-4">Add to Cart</Button>
+            <Button className="mt-2" onClick={() => addToCart({ ...product })}>
+              Add to Cart
+            </Button>
           </Card>
         ))}
       </div>
