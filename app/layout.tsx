b27cc7ed98faea46f572aad7877227c448e97702
taranom_y <tarannom.yousefi98@gmail.com>
2025-07-20 23:29:44 +0330

    fix : moving query client import to a seprated file

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 
-import { QueryClient } from "@tanstack/react-query";
 import Header from "@/components/ui/header";
+import { queryClient } from "@/lib/queryClient";
 
-export const queryClient = new QueryClient();
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
