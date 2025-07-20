65526c050c5ba5176edda319f82275b5dd991b3d
taranom_y <tarannom.yousefi98@gmail.com>
2025-07-20 22:18:04 +0330

    feat: import and render Header component in layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
 "use client";
 import FilteredProducts from "@/components/ui/filteredProducts";
-import Header from "@/components/ui/header";
 
 import ProductList from "@/components/ui/productList";
 
 export default function Home() {
   return (
     <>
-      <header className="fixed top-0 left-0 w-full z-50 bg-black shadow">
-        <Header />
-      </header>
-
-      <main className="pt-16">
+      <main className="pt-16 container flex flex-col  justify-center mx-auto">
         <FilteredProducts />
         <ProductList />
       </main>
